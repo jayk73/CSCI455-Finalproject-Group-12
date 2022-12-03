07b115fb4e6413ae25d4762f2bc798d7afccf488
David Sherven <jacksherven@yahoo.com>
2022-12-03 11:43:41 -0600

    Server.py working

--- server.py
+++ server.py
@@ -1,17 +1,51 @@
 
-import bluetooth
+import bluetooth, os
+from threading import Thread #Need thread to listen to stop waiting
 
-server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = 5
-server_sock.bind(("",port))
-server_sock.listen(1)
 
-client_sock,address = server_sock.accept()
-print ("Accepted connection from ",address)
+#Thread which listens on a port and then loops to 
+#recivee all messages from that conection
+#Thread ends when user enters any key
+class socketListener(Thread):
+    def run(self):
+        server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        port = 5
+        server_sock.bind(("",port))
+        server_sock.listen(1)
+        while True:
+            
+            client_sock,address = server_sock.accept()
+            print ("Accepted connection from ",address)
 
-data = client_sock.recv(1024)
-print ("received [%s]" % data)
+            data = client_sock.recv(1024)
+            print ("received [%s]" % data)
 
-client_sock.close()
-server_sock.close()
+
+#Start the thread and then kill the process
+#when any key is pressed
+pid = os.getpid()
+sl = socketListener()
+sl.start()
+input('Socket is listening, press any key to abort...')
+os.kill(pid,9)
+
+
+
+
+#Old code, delete later
+#server_sock.listen(1)
+# while True:
+#     try:
+#         client_sock,address = server_sock.accept()
+#         print ("Accepted connection from ",address)
+
+#         data = client_sock.recv(1024)
+#         print ("received [%s]" % data)
+#     except KeyboardInterrupt:
+        
+#         client_sock.close()
+#         server_sock.close()
+#         break
+
+
