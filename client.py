9ff2ea33d6a9f336cffeba1209f8ea9f44b42953
NDJackSparrow <100958691+NDJackSparrow@users.noreply.github.com>
2022-12-03 11:42:25 -0600

    Client.py working

--- client.py
+++ client.py
@@ -1,12 +1,50 @@
 import bluetooth
 
-bd_addr = "01:23:45:67:89:AB"
+print("Looking for nearby devices .... ")
+
+nearby_devices = bluetooth.discover_devices(
+    duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+
+print("found %d device(s)" % len(nearby_devices))
+
+my_list = []
+i = 1
+for addr, name in nearby_devices:    
+    try:
+        #print("  %s - %s" % (addr, name))
+        my_list.append(["INDEX: " + str(i), "NAME: " + name,  addr])
+        i += 1
+    except UnicodeEncodeError:
+        #print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+        my_list.append(["INDEX: " + str(i) , "NAME: " + name.encode('utf-8', 'replace'), addr])
+        i += 1
+
+for element in my_list:
+    print(element)
+
+choice = input("Select the index of the device you would like to communicate with ")
+choice = int(choice)
+
+choice = choice-1
+
+bd_addr = my_list[choice][2]
+
 
 port = 5
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+
+
 sock.connect((bd_addr, port))
 
-sock.send("hello!!")
+while True:
+    inp = input("Enter a message to send (enter 'quit' to quit):\n")
+    print(inp)
+    if (inp == "quit"):
+        break
+    else:
+        sock.send(inp)
+
 
 sock.close()
--- test.py
+++ test.py
@@ -10,14 +10,29 @@
 import bluetooth
 
 print("performing inquiry...")
-
 nearby_devices = bluetooth.discover_devices(
     duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
 
 print("found %d device(s)" % len(nearby_devices))
 
-for addr, name in nearby_devices:
+my_list = []
+i = 1
+for addr, name in nearby_devices:    
     try:
-        print("  %s - %s" % (addr, name))
+        #print("  %s - %s" % (addr, name))
+        my_list.append(["INDEX: " + str(i), "NAME: " + name,  addr])
+        i += 1
     except UnicodeEncodeError:
-        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+        #print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+        my_list.append(["INDEX: " + str(i) , "NAME: " + name.encode('utf-8', 'replace'), addr])
+        i += 1
+
+for element in my_list:
+    print(element)
+
+choice = input("Select the index of the device you would like to communicate with ")
+choice = int(choice)
+
+choice = choice-1
+
+print(my_list[choice][2])
