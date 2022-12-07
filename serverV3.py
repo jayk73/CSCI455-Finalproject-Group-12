0647b54f7d48fc86f76cea627c134c725278cace
David Sherven <jacksherven@yahoo.com>
2022-12-06 19:36:06 -0600

    Server V3 working still needs more work

--- serverV3.py
+++ serverV3.py
@@ -1,34 +1,38 @@
 
-import bluetooth, os, sys
-from _thread import * #Need thread to listen to stop waiting
-import threading
+# import bluetooth, os, sys
+# from _thread import * #Need thread to listen to stop waiting
+# import threading
+# from threading import Thread
+
+# server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+# ser = bluetooth.BluetoothSocket()
+
+
+# # port = bluetooth.get_available_port( bluetooth.RFCOMM )
+# # port = bluetooth.get_available_port( 0 )
+# # port = 5
+# server_sock.bind(("",bluetooth.PORT_ANY))
+
+
+# server_sock.listen(10) #Allows 10 active connections
+# port = server_sock.getsockname()[1]
+# print ("listening on port %d",port)
+
+# # uuid = "4C4C4544-0036-3910-8052-C7C04F344233"
+# uuid  = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+# # bluetooth.advertise_service( sock=server_sock, name= "Bob Service")
+# bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+#                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+#                             profiles=[bluetooth.SERIAL_PORT_PROFILE],
+#                             # protocols=[bluetooth.OBEX_UUID]
+#                             )
+
+
+import bluetooth, os
 from threading import Thread
-
-server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-ser = bluetooth.BluetoothSocket()
-
-
-# port = bluetooth.get_available_port( bluetooth.RFCOMM )
-# port = bluetooth.get_available_port( 0 )
-# port = 5
-server_sock.bind(("",bluetooth.PORT_ANY))
-
-
-server_sock.listen(10) #Allows 10 active connections
-port = server_sock.getsockname()[1]
-print ("listening on port %d",port)
-
-# uuid = "4C4C4544-0036-3910-8052-C7C04F344233"
-uuid  = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
-# bluetooth.advertise_service( sock=server_sock, name= "Bob Service")
-bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
-                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
-                            # protocols=[bluetooth.OBEX_UUID]
-                            )
-
+import threading
 list_of_clients = []
 def clientThread(connection, address):
     print("thread created")
@@ -40,7 +44,7 @@ def clientThread(connection, address):
             if data:
                 print ("<" + address[0] + "> " + data)
                 message_to_send = "<" + address[0] + "> " + data
-                broadcast(message_to_send, connection)
+                forward(message_to_send, connection)
             else:
                 remove(connection)
                 print("Removing connection")
@@ -48,11 +52,12 @@ def clientThread(connection, address):
         except:
             continue
 
-def broadcast(message, connection):
+def forward(message, connection):
     for clients in list_of_clients:
         if clients != connection:
             try:
-                clients.send(message).encode()
+                message = message.encode()
+                clients.send(message)
                 pass
             except:#for some reason always goes into except state, even when message is sent correctly
                 pass 
@@ -60,6 +65,14 @@ def broadcast(message, connection):
                 # print('removing clinet')
                 # #if link broken, remove client
                 # remove(clients)
+
+def broadcast(message):
+    for clients in list_of_clients:
+        try:
+            message = message.encode()
+            clients.send(message)
+        except:#for some reason always goes into except state, even when message is sent correctly
+                pass 
 
 def remove(connection):
     if connection in list_of_clients:
@@ -72,29 +85,54 @@ class socketListner(Thread):
             #Accepts connection request. Store paramteters 'connection' and 'address'
             #which is socket object for that user and bluettooth address they connected from
             connection, address = server_sock.accept()
-            
-            #Trying to get teh name of the connected device
-            # nearby_devices = bluetooth.discover_devices(
-            #     duration=2, lookup_names=True, flush_cache=True, lookup_class=False)
 
-            # connectionName = ""
-            # for devAddress, name in nearby_devices:
-            #     if address == devAddress:
-            #         connectionName = name
-            #         break
-            
-            
             #Maintain a list of clients so you can broadcast messages to all clients
             list_of_clients.append(connection)
 
             #When a user clonnects, print the address of that user
             print ( address[0] +  " connected")
-
+            broadcast (address[0] + " Connected to the server")
             # creates and individual thread for every user
             # that connects
             print("Creating thread")
             threading.Thread(target= clientThread, args=(connection,address) ).start()
             # start_new_thread(clientThread,(connection,address))
+
+# pid = os.getpid()
+# s1 = socketListner()
+# s1.start()
+
+# while True:
+#     message = input()
+#     if message:
+#         broadcast("<Server> " + message, "Bob")
+
+#!/usr/bin/env python3
+"""PyBluez simple example rfcomm-server.py
+Simple demonstration of a server application that uses RFCOMM sockets.
+Author: Albert Huang <albert@csail.mit.edu>
+$Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+"""
+
+
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(3)
+
+port = server_sock.getsockname()[1]
+
+# uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+uuid =  "0E448EB5-3425-ED48-91BE-1AED04C0512D"
+
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+
+print("Waiting for connection on RFCOMM channel", port)
 
 pid = os.getpid()
 s1 = socketListner()
@@ -102,10 +140,22 @@ s1.start()
 
 while True:
     message = input()
+    message = message.encode()
     if message:
-        broadcast("<Server> " + message, "Bob")
+        for clients in list_of_clients:
+            try:
+                message = message.encode()
+                clients.send(message)
+                pass
+            except:#for some reason always goes into except state, even when message is sent correctly
+                pass 
 
 
+print("Disconnected.")
+
+client_sock.close()
+server_sock.close()
+print("All done.")
         
 
 
