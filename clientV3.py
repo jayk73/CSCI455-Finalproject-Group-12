e6a6d73c15cc887305111e9dda65e36381ce5e50
NDJackSparrow <100958691+NDJackSparrow@users.noreply.github.com>
2022-12-06 19:35:40 -0600

    Client V3 Running, still needs more work

--- clientV3.py
+++ clientV3.py
@@ -6,13 +6,13 @@ class socketListener(Thread):
     def run(self):
         while True:
 
-            sockets_list = [bluetooth.BluetoothSocket(), server_sock]
+            sockets_list = [bluetooth.BluetoothSocket(), sock]
             try:
                 read_sockets, write_socket, error_socket = select.select(sockets_list,[],[])
             
                 for socks in read_sockets:
                     
-                    if socks == server_sock:
+                    if socks == sock:
                         message = socks.recv(2048).decode()
                         if message:
                             print(message)
@@ -42,15 +42,22 @@ i = 1
 for addr, name in nearby_devices:    
     try:
         #print("  %s - %s" % (addr, name))
-        my_list.append(["INDEX: " + str(i), "NAME: " + name,  addr])
+        my_list.append(["INDEX: " + str(i),  name,  addr])
         i += 1
     except UnicodeEncodeError:
         #print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
-        my_list.append(["INDEX: " + str(i) , "NAME: " + name.encode('utf-8', 'replace'), addr])
+        my_list.append(["INDEX: " + str(i) ,  name.encode('utf-8', 'replace'), addr])
         i += 1
 
 for element in my_list:
     print(element)
+    # print("Services Found: ")
+    # services = bluetooth.find_service( element[2] )
+    # if len(services) <=0:
+    #     print("no services found :( \n")
+    # else:
+    #     for serv in services:
+    #         print( serv['name'] + "\n")
 
 choice = input("Select the index of the device you would like to communicate with:   ")
 choice = int(choice)
@@ -58,54 +65,130 @@ choice = int(choice)
 choice = choice-1
 
 bd_addr = my_list[choice][2]
+bd_name = my_list[choice][1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-print("UID is " + uuid + " Looking on address " +bd_addr)
+connect_addr = bd_addr 
+connect_name = bd_name
 
-service_matches = bluetooth.find_service( uuid = uuid, address=bd_addr )
-print(uuid)
+# if len(sys.argv) < 2:
+#     print("No device specified. Searching all nearby bluetooth devices for "
+#           "the SampleServer service...")
+# else:
+#     connect_addr  = sys.argv[1]
+#     print("Searching for SampleServer on {}...".format(connect_addr ))
 
+# search for the SampleServer service
+# uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "0E448EB5-3425-ED48-91BE-1AED04C0512D"
+service_matches = bluetooth.find_service( address=connect_addr )
+
+first_match = None
 if len(service_matches) == 0:
-    print ("couldn't find the FooBar service")
+    print("Couldn't find the SampleServer service.")
+    sys.exit(0)
+else:
+    for ser in service_matches:
+        if  "SampleServer" in str(  ser["name"]   ) :
+            print("Found service: " + str(ser))
+            first_match = ser
+
+if first_match is None:
+    print("Couldn't find the SampleServer service.")
     sys.exit(0)
 
-first_match = service_matches[0]
+            
+# first_match = service_matches[0]
+
 port = first_match["port"]
 name = first_match["name"]
 host = first_match["host"]
 
-print ("connecting to \"%s\" on %s" % (name, host) )
 
-sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+print("Connecting to \"{}\" on {} with port {}".format(name, host, port))
+
+# Create the client socket
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
-sock.send("hello!!")
-sock.close()
-
-
-# port = 5 #arbitrary number, there is code to serach for an available port
-
-server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-
-server_sock.connect((bd_addr, port))
-
-server_sock.setblocking(False)
-
-#Start a thread to listen from the server while main continues to listen for user input
 
 pid = os.getpid()
 s1 = socketListener()
 s1.start()
 
 
+
+
+print("Connected. Type something...")
 while True:
-    message = input()
+    data = input()
+    if not data:
+        break
+    
+    sock.send(data)
 
-    if message:
-        message = message.encode()
-        server_sock.send(message)
-
-    #sockets_list = [bluetooth.BluetoothSocket(), server_sock]
+sock.close()
 
 
-server_sock.close()
+
+# uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+# print("UID is " + uuid + " Looking on address " +bd_addr)
+
+# service_matches = bluetooth.find_service( uuid = uuid, address=bd_addr )
+# print(uuid)
+
+# if len(service_matches) == 0:
+#     print ("couldn't find the FooBar service")
+#     sys.exit(0)
+
+# first_match = service_matches[0]
+# port = first_match["port"]
+# name = first_match["name"]
+# host = first_match["host"]
+
+# print ("connecting to \"%s\" on %s" % (name, host) )
+
+# sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+# sock.connect((host, port))
+# sock.send("hello!!")
+# sock.close()
+
+
+# # port = 5 #arbitrary number, there is code to serach for an available port
+
+# server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+
+# server_sock.connect((bd_addr, port))
+
+# server_sock.setblocking(False)
+
+# #Start a thread to listen from the server while main continues to listen for user input
+
+# pid = os.getpid()
+# s1 = socketListener()
+# s1.start()
+
+
+# while True:
+#     message = input()
+
+#     if message:
+#         message = message.encode()
+#         server_sock.send(message)
+
+#     #sockets_list = [bluetooth.BluetoothSocket(), server_sock]
+
+
+# server_sock.close()
+#!/usr/bin/env python3
+"""PyBluez simple example rfcomm-client.py
+Simple demonstration of a client application that uses RFCOMM sockets intended
+for use with rfcomm-server.
+Author: Albert Huang <albert@csail.mit.edu>
+$Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
+"""
+
+# import sys
+
+# import bluetooth
+
